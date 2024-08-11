b070b1f93d9865863b921610290b1d8121134a41
IOS2004 <lostlevy@gmail.com>
2024-08-11 21:11:39 +0530

    Simplified Product cards

--- public/scripts/javascript.js
+++ public/scripts/javascript.js
@@ -1,26 +1,56 @@
+/* populate scrollers */
+
 let scroller = document.querySelector('#bestSelling');
 let scroller2 = document.querySelector('#classic');
 let scroller3 = document.querySelector('#featured');
 
-function addItem(parent, name, path, price)
+function addItem(parent, name, path, price, page)
 {
   let item = document.createElement('div');
   item.classList.add('item-card');
-  item.innerHTML = '<img src="' + path + '" alt="' + name + '"> ' + name + '<div> &#8377; ' + price + '</div> <a href="#"> shop now &#8594; </a>';
+  item.innerHTML = '<img src="' + path + '" alt="' + name + '"> ' + name + '<div> &#8377; ' + price + '.00</div> <a href="'+ page + '"> shop now &#8594; </a>';
   parent.appendChild(item);
 }
 
-for (let i = 0; i < 6; i++)
+function createProductPage()
 {
-  addItem(scroller, "best selling headphone " + i, "public/images/bestselling/" + i + ".jpg", (i + 1) * 1000 );
+
+
+
+  return '#';
+}
+
+function getRandom(a, b)
+{
+  return Math.floor(Math.random() * (b - a + 1)) + a;
 }
 
 for (let i = 0; i < 6; i++)
 {
-  addItem(scroller2, "classic headphone " + i, "public/images/classics/" + i + ".jpg", (i + 1) * 1000 );
+  addItem(scroller, "best selling headphone " + i, "public/images/bestselling/" + i + ".jpg", getRandom(1000, 15000), createProductPage() );
+}
+
+for (let i = 0; i < 6; i++)
+{
+  addItem(scroller2, "classic headphone " + i, "public/images/classics/" + i + ".jpg",  getRandom(1000, 15000), createProductPage() );
 }
 
 for (let i = 0; i < 7; i++)
 {
-  addItem(scroller3, "featured headphone " + i, "public/images/featured/" + i + ".jpg", (i + 1) * 1000 );
+  addItem(scroller3, "featured headphone " + i, "public/images/featured/" + i + ".jpg",  getRandom(1000, 15000), createProductPage() );
 }
+
+/* Product page */
+let header = document.querySelector('#mainHeader');
+let footer = document.querySelector('#mainFooter');
+let test = document.querySelector("#test");
+test.addEventListener('click', () => {
+  const content = header.outerHTML;
+  const content2 = footer.outerHTML;
+  localStorage.setItem('header', content);
+  localStorage.setItem('footer', content2);
+  console.log(localStorage.getItem('header'));
+  console.log(localStorage.getItem('footer'));
+  prompt('hello');
+  window.location.href = 'public/pages/product_page.html';
+});
---
+++ public/scripts/productScript.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const head = localStorage.getItem('header');
+  const foot = localStorage.getItem('footer');
+  console.log(head);
+
+});
